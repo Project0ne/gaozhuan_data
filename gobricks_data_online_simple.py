b0ed936af67b3b43247e5d360d5555fb698a9ffa
Project0ne <40708415+Project0ne@users.noreply.github.com>
2023-04-26 21:13:39 +0800

    Update gobricks_data_online_simple.py

--- gobricks_data_online_simple.py
+++ gobricks_data_online_simple.py
@@ -1,6 +1,5 @@
 import urllib3
 urllib3.disable_warnings()
-
 import csv
 import pandas as pd
 import requests
@@ -16,10 +15,8 @@ ids = df[0].tolist()
 
 # 定义 CSV 文件的字段名
 fieldnames = ["id", "product_id", "caption", "picture", "pictures", "eshop_price", "price", "caption_en", "color_id", "ldd_catalog", "inventory", "ldraw_no","ldd_code", "sale_volume", "rand"]
-
 # 增加 color_data 里面的字段名
-color_data_fieldnames = ["main_id","id", "name", "lego_color_id", "font-color", "color", "colorType", "ldraw_color_id", "ldraw_color_value", "index", "name_en"]
-
+color_data_fieldnames = ["main_id","name", "lego_color_id", "font-color", "color", "colorType", "ldraw_color_id", "ldraw_color_value", "index", "name_en"]
 # 合并所有的字段名
 all_fieldnames = fieldnames + color_data_fieldnames
 
@@ -29,28 +26,25 @@ filename = "gobrick_data_" + time.strftime("%Y-%m-%d_%H-%M-%S") + ".csv"
 def fetch_data(id):
     # 要从中获取数据的 URL
     url = f'https://gobricks.cn/frontend/v1/item/filter?product_id={id}&type=2&limit=96&offset=0'
-
     try:
         # 发送 GET 请求到 URL 并存储响应
         response = requests.get(url, verify=False)
     except requests.exceptions.ProxyError as e:
         print("ProxyError occurred:", e)
         return []
-
     # 仅提取我们想要写入 CSV 文件的字段
     data = response.json()['rows']
     filtered_data = []
     for d in data:
-        # 把 color_data 里面的键值对也加入到 d 里面，但是避免替换掉原来的 id
+        # 把 color_data 里面的键值对也加入到 d 里面
         for k, v in d['color_data'].items():
-            if k != "id":
-                d[k] = v
+            d[k] = v
         # 把 d 中只包含我们需要的字段的部分保存到 filtered_data 里面
         filtered_data.append({k: v for k, v in d.items() if k in all_fieldnames})
-    
+        # 把 ldraw_no 对应的数值统一用逗号分隔
+        filtered_data[-1]['ldraw_no'] = filtered_data[-1]['ldraw_no'].replace(' ', ',').replace(';', ',')
     # 更新进度条
     pbar.update(1)
-    
     return filtered_data
 
 with concurrent.futures.ThreadPoolExecutor() as executor:
@@ -60,17 +54,11 @@ with concurrent.futures.ThreadPoolExecutor() as executor:
         # 仅在文件为空时写入标题
         if csvfile.tell() == 0:
             writer.writeheader()
-        
         # 初始化进度条
         pbar = tqdm(total=len(ids))
-        
         # 将请求提交给执行器并将数据写入 CSV 文件
         for data in executor.map(fetch_data, ids):
             for d in data:
-                # 如果 color_id 不足三位数，就在前面补零
-                if len(d["color_id"]) < 3:
-                    d["color_id"] = d["color_id"].zfill(3)
                 writer.writerow(d)
-        
         # 关闭进度条
         pbar.close()
